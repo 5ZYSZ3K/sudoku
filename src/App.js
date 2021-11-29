1ab9a13822401f0d3347d14c95823cded413f72c
5ZYSZ3K <paw.por@wp.pl>
2021-11-29 13:02:57 +0100

    removed useless code (at least i hope it was)

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       let x = (Math.floor(Math.random()*9));
       let y = (Math.floor(Math.random()*9));
       let v = (Math.floor(Math.random()*9)+1);
-      while(!isMoveValid(startingFill, [x,y], v)[0]){
+      while(!isMoveValid(startingFill, [x,y], v)){
         x = (Math.floor(Math.random()*9));
         y = (Math.floor(Math.random()*9));
         v = (Math.floor(Math.random()*9)+1);
@@ -40,7 +40,7 @@ function App() {
       {
         currentFill.map((data,i) => {
           const JSX = data.map((data1,j) => {
-            return <Cell key={`${i} ${j}`} coords={[i,j]} readonly={startFill[i][j]?true:false} inputHandler={handler} startingValue={data1} color={isMoveValid(currentFill, [i,j], data1)[0]?"#fff":"#faa"} />
+            return <Cell key={`${i} ${j}`} coords={[i,j]} readonly={startFill[i][j]?true:false} inputHandler={handler} startingValue={data1} color={isMoveValid(currentFill, [i,j], data1)?"#fff":"#faa"} />
           })
           return JSX;
         })
@@ -49,21 +49,18 @@ function App() {
   );
 }
 function isMoveValid(a, coords, value) {
-  const arrayOfMistakes = []
-  const arrayOfGoodMoves = []
+  const arrayOfMistakes = [];
+  const arrayOfGoodMoves = [];
+  if(value === "") return true;
   for(let i=0; i<9; i++){
-    if((i!==coords[0]) && (value === a[i][coords[1]])) arrayOfMistakes.push([i,coords[1]]);
-    else if  ((i!==coords[1]) && (value === a[coords[0]][i])) arrayOfMistakes.push([coords[0],i]);
-    else arrayOfGoodMoves.push([i,coords[1]], [coords[0],i]);
+    if((i!==coords[0]) && (value === a[i][coords[1]])) return false;
+    else if  ((i!==coords[1]) && (value === a[coords[0]][i])) return false;
   }
   for(let i=coords[0]-coords[0]%3; i<coords[0]-coords[0]%3+3; i++){
     for(let j=coords[1]-coords[1]%3; j<coords[1]-coords[1]%3+3; j++){
-      if(!(i===coords[0] && j===coords[1]) && value === a[i][j]) arrayOfMistakes.push([i, j]);
-      else arrayOfGoodMoves.push([i, j]);
+      if(!(i===coords[0] && j===coords[1]) && value === a[i][j]) return false;
     }
   }
-  if(value === "") return [true, [],arrayOfGoodMoves]
-  else if(arrayOfMistakes.length === 0) return [true, arrayOfMistakes, arrayOfGoodMoves]
-  else return [false, arrayOfMistakes, arrayOfGoodMoves]
+  return true;
 }
 export default App;
